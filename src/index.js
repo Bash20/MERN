d20c0ccea9c4538d590d4e52dfe0dbbf0c4fa650
Brady Ash <bradyash9@gmail.com>
2022-11-01 12:56:21 -0600

    Error Fix 1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import MyCounter from './components/MyCounter';
-import MyGitHub from '/.components/MyGitHub';
+import MyGitHub from './components/MyGitHub';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
